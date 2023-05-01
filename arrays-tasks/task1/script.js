993bf3792e296c27716054c3344a9570f585d500
AlexanderBotnari <sandubotnari99@gmail.com>
2023-05-01 12:34:19 +0300

    init simple bank app

---
+++ arrays-tasks/task1/script.js
@@ -0,0 +1,23 @@
+const janeData = [4, 5, 3, 11, 6, 2, 4, 1, 5, 9];
+const juliaData = [2, 4, 5, 1, 13, 2, 15, 8, 3, 7];
+
+const janeData1 = [3, 5, 9, 14, 1, 2, 6, 8, 3, 10];
+const juliaData1 = [8, 2, 10, 1, 2, 5, 6, 3, 1, 4];
+
+const verifyCats = function (catsJane, catsJulia) {
+    const catsJaneCopy = catsJane.slice(1, -1);
+    const catsJuliaCopy = catsJulia.slice(1, -1);
+
+    const allCats = catsJaneCopy.concat(catsJuliaCopy);
+
+    allCats.forEach(function (age, nr) {
+        if(age >= 2){
+            console.log(`Кошка № ${nr + 1} взрослая, ей ${age} лет`);
+        }else{
+            console.log(`Кошка № ${nr + 1} ещё котёнок`);
+        }
+    });
+};
+
+// verifyCats(janeData, juliaData);
+verifyCats(janeData1, juliaData1);
