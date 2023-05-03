0671dcd5d5cc4898eff89f74413db31af92f7629
Alexandru <sandubotnari99@gmail.com>
2023-05-03 13:38:06 +0300

    added array task 3

---
+++ arrays-tasks/task3/app.js
@@ -0,0 +1,8 @@
+//Folosind metoda filter(), afișați numerele impare dintr-un array de numere.
+// const arr = [3, 1, 9, 4, 5, 10];
+// console.log(arr.filter(value => value % 2 !== 0));
+
+//Folosind metoda map(), creați un nou array care să conțină dublul fiecărui element dintr-un array dat.
+// const arr = [3, 1, 9, 4, 5, 10];
+// console.log(arr.map(value => value * 2));
+
