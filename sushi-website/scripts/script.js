71f07c74083fbca6590e62062055f73c62c37e2b
Alexandru <sandubotnari99@gmail.com>
2023-06-19 12:55:03 +0300

    added animations

--- sushi-website/scripts/script.js
+++ sushi-website/scripts/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+    //sticky nav
    const waypoint = new Waypoint({
     element: document.getElementById('futures-section'),
     handler: function(direction) {
@@ -9,5 +11,88 @@ $(document).ready(function(){
       }
     },
     offset: 60,
-   })
+   });
+
+   //buttons
+   $('.order-button').click(function(){
+      $('html, body').animate({scrollTop: $('#how-to-order-section').offset().top}, 1000);
+   });
+
+   $('.more-button').click(function(){
+    $('html, body').animate({scrollTop: $('#futures-section').offset().top}, 1000);
+   });
+
+      // Select all links with hashes
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
+      }
+    });
+
+    //animations
+    const waypoint1 = new Waypoint({
+      element: document.getElementById('futures-section'),
+      handler: function(direction) {
+        $('.anim1').addClass('animate__animated animate__fadeIn');
+      },
+      offset: 60,
+     });
+
+     const waypoint2 = new Waypoint({
+      element: document.getElementById('cities-section'),
+      handler: function(direction) {
+        $('.anim2').addClass('animate__animated animate__fadeInRight');
+      },
+      offset: 60,
+     });
+
+     const waypoint3 = new Waypoint({
+      element: document.getElementById('how-to-order-section'),
+      handler: function(direction) {
+        $('.anim3').addClass('animate__animated animate__fadeInUp');
+      },
+      offset: 60,
+     });
+
+     const waypoint4 = new Waypoint({
+      element: document.getElementById('header-text'),
+      handler: function(direction) {
+        if(direction == 'up'){
+          $('.anim4').addClass('animate__animated animate__wobble');
+        }else{
+          $('.anim4').removeClass('animate__animated animate__wobble');
+        }
+      },
+      offset: 60,
+     });
 })
