53261bf6ce9a8c463f731fa15aa08acba0bf019a
AlexanderBotnari <sandubotnari99@gmail.com>
2023-07-04 19:48:01 +0300

    loan calculator

---
+++ test/loan-calculator/calculator.js
@@ -0,0 +1,4 @@
+document.querySelector('.input-submit').addEventListener('click' ,function(){
+   const sum = document.querySelector('.input-sum').value;
+   alert(`Ati imprumutat ${sum} lei .`);
+});
